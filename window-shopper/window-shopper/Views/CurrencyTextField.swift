ab49b28e781aff1a017bde32d71072bf22df6ad9
Luke Shore <LukeShore@Lukes-MacBook-Air.local>
2018-03-15 22:57:57 +1100

    text field added

---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Luke Shore on 15/3/18.
+//  Copyright © 2018 Luke Shore. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2472174658)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+        
+        
+    }
+
+}
