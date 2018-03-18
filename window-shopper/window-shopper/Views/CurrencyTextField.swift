f1bcea13199abbff135ce98276cd83d88043ed88
Luke Shore <LukeShore@Lukes-MacBook-Air.local>
2018-03-18 18:56:26 +1100

    added button with code

--- window-shopper/window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -8,22 +8,31 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
+    
 
+    override func prepareForInterfaceBuilder() {
+        customizedView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2472174658)
-        layer.cornerRadius = 5.0
-        textAlignment = .center
-        
-        
-        if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
-            attributedPlaceholder = place
-            textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        customizedView()
         }
         
-        
+        func customizedView() {
+            backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2472174658)
+            layer.cornerRadius = 5.0
+            textAlignment = .center
+            
+            
+            
+            if let p = placeholder {
+                let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+                attributedPlaceholder = place
+                textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
     }
 
 }
